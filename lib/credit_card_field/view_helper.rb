68a0887512f548d2896f39b0aa73839d7d902c7a
benzhang <bzbnhang@gmail.com>
2012-11-30 13:38:09 +1030

    rm div

--- lib/credit_card_field/view_helper.rb
+++ lib/credit_card_field/view_helper.rb
@@ -4,16 +4,11 @@ module CreditCardField
     def credit_card_type_field(method, options = {})
       options[:card_number_selector] ||= "##{@object_name}_credit_card_no"
       options[:accept_types] ||= %w(visa master american_express diners_club)
-      html = %Q(<div class="control-group">
-          <div class="controls">
-            <ul class="credit-card-type clearfix">
-      ).html_safe
+      html = '<ul class="credit-card-type clearfix">'.html_safe
       options[:accept_types].each {|type| html << "<li class=\"icon #{type} #{'active' if self.object.try(method) == type}\">#{type.camelize}</li>".html_safe }
       html << %Q(<li class="icon not_valid #{'active' unless self.object.try(method)}">Invalid Card</li>
               <li class="hint not_valid"><strong>Not a Valid Credit Card Number</strong></li>
-            </ul>
-          </div>
-        </div>).html_safe
+            </ul>).html_safe
       html << hidden_field(method, rel: 'credit-card-type')
       html << %Q(
         <script>
