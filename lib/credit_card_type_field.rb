634084dac30866ea78cb1f554186ae6d614df10f
Ben Zhang <bzbnhang@gmail.com>
2013-01-16 10:39:21 +1030

    add tag helper

--- lib/credit_card_type_field.rb
+++ lib/credit_card_type_field.rb
@@ -7,13 +7,14 @@ module CreditCardField
       ActiveSupport.on_load(:action_view) do
         require "credit_card_type_field/view_helper"
         class ActionView::Helpers::FormBuilder
-          include CreditCardTypeField::ViewHelper
+          include CreditCardTypeField::ViewHelper::FormHelper
         end
+        ActionView::Base.send :include, CreditCardTypeField::ViewHelper::FormTagHelper
 
         begin
           require 'simple_form'
           class SimpleForm::FormBuilder
-            include CreditCardTypeField::ViewHelper
+            include CreditCardTypeField::ViewHelper::FormHelper
           end
         rescue LoadError
         end
--- lib/credit_card_type_field/version.rb
+++ lib/credit_card_type_field/version.rb
@@ -1,3 +1,3 @@
 module CreditCardField
-  VERSION = "1.0"
+  VERSION = "1.2"
 end
